4aaeba26a98fbf37b9c4085a77c1719a4c14b85d
yonib98 <battat@campus.technion.ac.il>
2022-01-18 13:43:27 +0200

    Files

---
+++ test.s
@@ -0,0 +1,16 @@
+.text
+main:   # Start of your code
+
+        # End of your code
+        add		t6, x0, x0
+        beq		t6, x0, finish
+
+deadend: beq	t6, x0, deadend        
+
+finish:
+        lw		t4, 0(x0)
+        lw		t5, 4(x0)
+        sw		t5, 0xFF(t4)
+        beq		t6, x0, deadend
+
+
